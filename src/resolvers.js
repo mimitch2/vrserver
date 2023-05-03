e3c8ac114bff43e0cfc7d0f91c22b96ae9bbaef2
mimitch2 <miike@lawnstarter.com>
2023-05-03 12:39:07 -0500

    feat(collection): new types for collection

--- src/resolvers.js
+++ src/resolvers.js
@@ -353,6 +353,7 @@ const getReleaseInCollection = async (__, { id }, context) => {
         );
 
         const result = await response.json();
+        console.log('🚀 ~ file: resolvers.js:356 ~ getReleaseInCollection ~ result:', result);
 
         return {
             isInCollection: !!result?.releases?.length ?? false,
