4e500b2bc3ce6e616b432b75d235cc5037b16318
mimitch2 <miike@lawnstarter.com>
2023-05-07 11:33:39 -0500

    feat(ts): kinda working for ts

--- dist/index.js
+++ dist/index.js
@@ -11,19 +11,15 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { getDiscogsHeadersAndUsername } from './src/helpers/helpers.js';
 import authRouter from './src/Routes/auth.routes.js';
 import { resolvers } from './src/resolvers.js';
-
 // import cors from 'cors';
 // import bodyparser from 'body-parser';
-
 // const key = readFileSync('./src/certs/selfsigned.key');
 // const cert = readFileSync('./src/certs/selfsigned.crt');
 // const options = {
 //     key,
 //     cert
 // };
-
 mongoose.Promise = global.Promise;
-
 const url = process.env.MONGO_DB;
 // @ts-ignore
 mongoose.connect(url, { useNewUrlParser: true });
@@ -31,7 +27,6 @@ mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}
 // @ts-ignore
 const typeDefs = readFileSync('./src/typeDefs.graphql', 'UTF-8');
 const app = express();
-
 // app.use(bodyparser);
 // app.use(authRouter);
 // app.use((req, res, next) => {
@@ -41,7 +36,6 @@ const app = express();
 //     next();
 // });
 // app.use(cors({ origin: ['http://localhost:3000', 'https://vinylratings.com'] }));
-
 const httpServer = http.createServer(app);
 const server = new ApolloServer({
     typeDefs,
@@ -51,36 +45,23 @@ const server = new ApolloServer({
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     status400ForVariableCoercionErrors: true,
 });
-
 await server.start();
-
-app.use(
-    '/graphql',
-    cors(),
-    authRouter,
-    bodyParser.json(),
-    expressMiddleware(server, {
-        // @ts-ignore
-        context: ({ req }) => {
-            const auth = req?.headers?.authorization ?? '';
-
-            if (auth) {
-                const authJson = auth.split(' ')[1];
-                const parsedAuth = JSON.parse(authJson);
-                const { username, Authorization } = getDiscogsHeadersAndUsername({
-                    auth: parsedAuth,
-                });
-
-                return { username, Authorization };
-            }
-
-            throw new GraphQLError('You must be logged in');
-        },
-    })
-);
-
+app.use('/graphql', cors(), authRouter, bodyParser.json(), expressMiddleware(server, {
+    // @ts-ignore
+    context: ({ req }) => {
+        const auth = req?.headers?.authorization ?? '';
+        if (auth) {
+            const authJson = auth.split(' ')[1];
+            const parsedAuth = JSON.parse(authJson);
+            const { username, Authorization } = getDiscogsHeadersAndUsername({
+                auth: parsedAuth,
+            });
+            return { username, Authorization };
+        }
+        throw new GraphQLError('You must be logged in');
+    },
+}));
 const PORT = process.env.PORT || 8080;
-
 httpServer.listen(PORT, () => {
     console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
 });
---
+++ dist/src/Schemas/Release.schema.js
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+import Float from 'mongoose-float';
+const { Schema, model } = mongoose;
+const FloatType = Float.loadType(mongoose);
+const releaseSchema = new Schema({
+    releaseId: {
+        type: Number,
+        required: true,
+    },
+    artist: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    ratingsCount: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    ratingAvg: {
+        type: FloatType,
+        required: true,
+        default: 0,
+    },
+    quietnessAvg: {
+        type: FloatType,
+        required: true,
+        default: 0,
+    },
+    flatnessAvg: {
+        type: FloatType,
+        required: true,
+        default: 0,
+    },
+    clarityAvg: {
+        type: FloatType,
+        required: true,
+        default: 0,
+    },
+}, { timestamps: true });
+releaseSchema.virtual('vinylRatings', {
+    ref: 'Rating',
+    localField: '_id',
+    foreignField: 'release',
+});
+releaseSchema.virtual('currentUserRating', {
+    ref: 'Rating',
+    localField: '_id',
+    foreignField: 'release',
+});
+releaseSchema.virtual('userCopy', {
+    ref: 'UserCopy',
+    localField: '_id',
+    foreignField: 'release',
+});
+releaseSchema.set('toObject', { virtuals: true });
+releaseSchema.set('toJSON', { virtuals: true });
+export default model('Release', releaseSchema);
